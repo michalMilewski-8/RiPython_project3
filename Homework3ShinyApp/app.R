2009a580282cbfac69db578970d1bd11b1a37556
HubertNastaly <h.nastaly@gmail.com>
2020-05-22 20:03:47 +0200

    First simple app. Not polished yet.

--- Homework3ShinyApp/app.R
+++ Homework3ShinyApp/app.R
@@ -1,47 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+---
+output: html_document
+runtime: shiny
+---
 
 library(shiny)
+library(leaflet)
+library(dplyr)
+
+workplacesNY <- read.csv("csv_results/workplaces_NY_2019.csv")
+# workplacesJC <- read.csv("csv_results/workplaces_JC_2019.csv")
+# spendingFreeTimeNY <- read.csv("csv_results/spendingFreeTime_NY_2019.csv")
+# spendingFreeTimeJC <- read.csv("csv_results/spendingFreeTime_JC_2019.csv")
+# hiringByAgeNY <- read.csv("csv_results/hiringByAge_NY_2019.csv")
+# hiringByAgeJC <- read.csv("csv_results/hiringByAge_JC_2019.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Project"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("hires",
+                        "Who:",
+                        list(`All` = "totalHires", `Men` = "manHires", `Women` = "womanHires"))
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           leafletOutput("workspacesNYLeaflet")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    colorpal <- reactive({
+        colorNumeric(palette = colorRamp(c("#0052D4","#65C7F7","#9CECFB"), interpolate = "spline"),
+                     domain = workplacesNY[[input$hires]])
+    })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$workspacesNYLeaflet <- renderLeaflet({
+        pal = colorpal()
+        leaflet(workplacesNY) %>%
+            setView(lat = 40.730610, lng = -73.935242, zoom = 11) %>%
+            addProviderTiles(providers$CartoDB.DarkMatter) %>%
+            fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
+            # addCircles(lat = ~ latitude, lng = ~ longitude,
+            #            weight = 1, radius = ~sqrt(totalHires), label = ~stationName,
+            #            color = ~ pal(workplacesNY[[input$hires]]), fillOpacity = 1.0)
+    })
+    
+    observe({
+        pal = colorpal()
+        leafletProxy("workspacesNYLeaflet", data = workplacesNY) %>%
+            addCircles(lat = ~ latitude, lng = ~ longitude,
+                       weight = 1, radius = ~sqrt(totalHires), label = ~stationName,
+                       color = pal(workplacesNY[[input$hires]]), fillOpacity = 1.0)
     })
 }
 
---
+++ R_scripts/processData.R
@@ -0,0 +1,111 @@
+library(dplyr)
+library(lubridate)
+library(data.table)
+
+# setwd("Documents/R_i_Python/Praca_domowa_3/RiPython_project3")
+
+mergeCsv <- function(dirPath,outputName){
+  list.files(path = dirPath, pattern = "*.csv", full.names = TRUE) %>%
+    lapply(function(fileName) read.csv(fileName, stringsAsFactors = FALSE)) %>%
+    rbindlist() -> merged
+  write.csv(merged, file = paste(c("csv_source/csv_merged/",outputName,".csv"), collapse = ''))
+}
+
+workplaces <- function(DF){
+  DF %>%
+    mutate(weekday = format(as.Date(stoptime), "%u")) %>%
+    filter(weekday >= 1 & weekday <= 5) %>%
+    filter(hour(stoptime) < 10) %>%
+    group_by(end.station.id) %>%
+    summarise(end.station.name[1], 
+              end.station.latitude[1], 
+              end.station.longitude[1], 
+              hires = n(), 
+              manHires = sum(gender == 1), 
+              womanHires = sum(gender == 2)) %>%
+    arrange(desc(hires)) -> Res
+  
+  colnames(Res) <- c("stationId", "stationName", "latitude", "longitude", "totalHires", "manHires", "womanHires")
+  return(Res)
+}
+
+processWorkplaces <- function(){
+  DF <- read.csv("csv_source/csv_merged/merged_JC_2019.csv")
+  DF <- workplaces(DF)
+  write.csv(DF,file="csv_results/workplaces_JC_2019.csv")
+  print("Zapisano JC")
+  
+  DF <- read.csv("csv_source/csv_merged/merged_NY_2019.csv")
+  DF <- workplaces(DF)
+  write.csv(DF,file="csv_results/workplaces_NY_2019.csv")
+  print("Zapisano NY")
+}
+
+hiringByAge <- function(DF){
+  currentYear <- as.integer(format(Sys.Date(), "%Y"))
+  DF %>%
+    filter(hour(stoptime) < 10) %>%
+    mutate(age = case_when(
+      currentYear - birth.year < 20 ~ 20,
+      currentYear - birth.year < 30 ~ 30,
+      currentYear - birth.year < 40 ~ 40,
+      currentYear - birth.year < 50 ~ 50,
+      currentYear - birth.year < 60 ~ 60,
+      currentYear - birth.year < 70 ~ 70,
+      currentYear - birth.year < 80 ~ 80,
+      TRUE ~ 0
+    )) %>%
+    group_by(end.station.id) %>%
+      summarise(end.station.name[1], 
+                end.station.latitude[1], 
+                end.station.longitude[1], 
+                under20 = sum(age == 20),
+                under30 = sum(age == 30),
+                under40 = sum(age == 40),
+                under50 = sum(age == 50),
+                under60 = sum(age == 60),
+                under70 = sum(age == 70),
+                under80 = sum(age == 80)) -> Res
+  
+  colnames(Res)[1:4] <- c("stationId", "stationName", "latitude", "longitude")
+  return(Res)
+}
+
+processHiringByAge <- function(){
+  DF <- read.csv("csv_source/csv_merged/merged_JC_2019.csv")
+  DF <- hiringByAge(DF)
+  write.csv(DF,file="csv_results/hiringByAge_JC_2019.csv")
+  print("Zapisano JC")
+  
+  DF <- read.csv("csv_source/csv_merged/merged_NY_2019.csv")
+  DF <- hiringByAge(DF)
+  write.csv(DF,file="csv_results/hiringByAge_NY_2019.csv")
+  print("Zapisano NY")
+}
+
+spendingFreeTime <- function(DF){
+  DF %>%
+    filter(hour(stoptime) > 10 & hour(stoptime) < 16) %>%
+    mutate(weekday = format(as.Date(stoptime), "%u")) %>%
+    filter(weekday >= 6) %>%
+    group_by(end.station.id) %>%
+    summarise(end.station.name[1], 
+              end.station.latitude[1], 
+              end.station.longitude[1], 
+              hires = n()) -> Res
+  
+  colnames(Res) <- c("stationId", "stationName", "latitude", "longitude", "hires")
+  return(Res)
+}
+
+processSpendingFreeTime <- function(){
+  DF <- read.csv("csv_source/csv_merged/merged_JC_2019.csv")
+  DF <- spendingFreeTime(DF)
+  write.csv(DF,file="csv_results/spendingFreeTime_JC_2019.csv")
+  print("Zapisano JC")
+  
+  DF <- read.csv("csv_source/csv_merged/merged_NY_2019.csv")
+  DF <- spendingFreeTime(DF)
+  write.csv(DF,file="csv_results/spendingFreeTime_NY_2019.csv")
+  print("Zapisano NY")
+}
